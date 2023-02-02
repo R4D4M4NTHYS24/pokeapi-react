17e51323df782f80c00e681e688dcf2662aa472f
Camilo Restrepo <camilorg2017@gmail.com>
2023-02-02 11:59:00 -0500

    create pokeapi project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    //Es un hoook
+
+    const fetchPokemons = async () => {
+      try {
+        const res = await fetch(
+          "https://pokeapi.co/api/v2/pokemon?limit=151&offset=12"
+        );
+
+        const data = await res.json();
+        console.log(data);
+
+        if (!res.ok) throw new Error(`conection API error status${res.status}`);
+      } catch (err) {
+        alert(`${err} please contact the administrator or try later...`);
+      }
+    };
+
+    fetchPokemons();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
