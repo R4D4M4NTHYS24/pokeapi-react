1b29b85aa2efce00b206443c26a5fec22d442212
Camilo Restrepo <camilorg2017@gmail.com>
2023-02-03 00:39:27 -0500

    conect api and create search bar component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import SearchBox from "./components/search-box/search-box";
 
 function App() {
+  const [searchField, setSearchField] = useState(""); //[value, setValue]
+  const [pokemons, setPokemons] = useState([]);
+  const [filteredPokemons, setFilterPokemons] = useState(pokemons);
+
   useEffect(() => {
     //Es un hoook
 
     const fetchPokemons = async () => {
       try {
         const res = await fetch(
-          "https://pokeapi.co/api/v2/pokemon?limit=151&offset=12"
+          "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0"
         );
 
-        const data = await res.json();
-        console.log(data);
+        const pokemons = await res.json();
+
+        setPokemons(pokemons.results);
 
         if (!res.ok) throw new Error(`conection API error status${res.status}`);
       } catch (err) {
@@ -25,9 +31,29 @@ function App() {
     fetchPokemons();
   }, []);
 
+  useEffect(() => {
+    const newFilteredPokemons = pokemons.filter((pokemon) => {
+      return pokemon.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    setFilterPokemons(newFilteredPokemons);
+    console.log(newFilteredPokemons);
+  }, [pokemons, searchField]);
+
+  const onSearchChange = (ChangeEvent) => {
+    const searchFieldString = ChangeEvent.target.value.toLocaleLowerCase();
+    //console.log(searchFieldString);
+    setSearchField(searchFieldString);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
+        <SearchBox
+          className="search-box"
+          onChangeHandler={onSearchChange}
+          placeholder={"search pokemons"}
+        />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
