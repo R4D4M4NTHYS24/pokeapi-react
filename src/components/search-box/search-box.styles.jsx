2ae5a4aafe27e2edbb376dda16d4adbc7d22704a
Camilo Restrepo <camilorg2017@gmail.com>
2023-02-08 08:56:46 -0500

    update css for search box

--- src/components/search-box/search-box.styles.jsx
+++ src/components/search-box/search-box.styles.jsx
@@ -1,21 +1,20 @@
 import styled from "styled-components";
 
 export const StyledInput = styled.input`
-  &.search-box {
-    width: 20rem;
-    margin: 2vh 40vw;
-    background-color: #fff;
-    border: 1px solid #ddd;
-    border-radius: 5px;
-    padding: 10px 15px;
-    font-size: 14px;
+  width: 50%;
+  margin: 2vh 25%;
+  background-color: #fff;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 10px 15px;
+  font-size: 14px;
 
-    &:focus {
-      outline: none;
-      border-color: #aaa;
-    }
-    @media screen and (max-width: 800px) {
-      margin: 2vh 10vw;
-    }
+  &:focus {
+    outline: none;
+    border-color: #aaa;
+  }
+  @media screen and (max-width: 800px) {
+    width: 50%;
+    margin: 2vh 25%;
   }
 `;
